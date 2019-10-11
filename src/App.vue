2e2e5119730d11846c4af2230a29be1e8286537e
ZhiHeng Zhong <zhongzhiheng@sunlands.com>
2019-10-11 12:35:06 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="warpper">
-      <tntPlayer src='https://gss3.baidu.com/6LZ0ej3k1Qd3ote6lo7D0j9wehsv/tieba-smallvideo/3_d97abb65fc36a1d689efd3a105bebce1.mp4' />
+      <tntPlayer src='http://valipl.cp31.ott.cibntv.net/69761e385a138716df4246df1/03000801005D48E3B7CAB3A003E880CF7F51BC-F11F-492D-BEA1-7C2EF0101627.mp4?ccode=0502&duration=6151&expire=18000&psid=92ba6084f0cc6928b1a3025d2602514f&ups_client_netip=0e12ecaf&ups_ts=1569805573&ups_userid=&utid=ygHFEhflVFsCAXeDmKvjJxyH&vid=XNDI1ODU2NzYwOA%3D%3D&vkey=Ac33895213967107ed8727d74312a8d8d' />
     </div>
 
   </div>
